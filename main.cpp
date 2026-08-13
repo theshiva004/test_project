b9d3b44e6fe8d5b3f9034e479ddc302e0d18baf0
theshiva004 <theshivshankar004@gmail.com>
2026-08-14 00:20:18 +0530

    Add calculator feature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    cout<<"Hello! This is git and github test.:)"<<endl;
+    //cout<<"Hello! This is git and github test.:)"<<endl;
+
+    cout<< "calculator feature was added."<<endl;
     return 0;
 }
